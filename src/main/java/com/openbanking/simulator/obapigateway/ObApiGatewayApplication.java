15ad55f27295a04b22a6774b72fcfcf28a149da1
Madan Narra <narra.madan@outlook.com>
2020-09-22 07:40:58 +1000

    Initial Commit

---
+++ src/main/java/com/openbanking/simulator/obapigateway/ObApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.openbanking.simulator.obapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openbanking/simulator/obapigateway/ObApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openbanking.simulator.obapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
